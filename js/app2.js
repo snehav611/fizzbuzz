39ed586de7ea3bded3aa65b3c7c194d14664539c
snehav611 <snehav611@gmail.com>
2014-10-05 18:10:41 -0400

    final

--- js/app2.js
+++ js/app2.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
+	do {
+		var number = +prompt("Please enter an integer starting from 1 to start Fizz Buzz:");
+	}
+	while(isNaN(number) || number % 1 != 0 || number <= 0);
+	var fizzBuzz = function(number) {
 	var count = 1;
-	for (count = 1; count <= 100; count++) {
+	for (count = 1; count <= number; count++) {
 	    if (count % 15 == 0) {
 	       $('ul').append('<li>' + 'Fizz Buzz' + '</li>');
 	    } else if (count % 5 == 0) {
@@ -11,5 +16,6 @@ $(document).ready(function() {
 	       $('ul').append('<li>' + count + '</li>')
 	    }
 	};
+	};
+	fizzBuzz(number);
 });
-
